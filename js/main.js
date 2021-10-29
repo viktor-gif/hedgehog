d168f7095c5efbc4eb27392b51768b1b34b876e4
viktor-gif <klim.victor.ua@gmail.com>
2021-10-29 17:00:57 +0300

    hedgehog wait

--- js/main.js
+++ js/main.js
@@ -3,19 +3,23 @@ console.log(window.innerHeight);
 
 let hedg = document.getElementById('hedgehog');
 
+setTimeout(() => {
+    const hedgWait = document.querySelector('.hedgehog__wait');
+    hedgWait.classList.add('hedgehog__wait_remove');
+}, 4000);
 
 function animateHedg() {
     hedg.animate([
         {
-            top: (Math.random() * window.innerHeight - 200) + 'px',
+            top: (Math.random() * (window.innerHeight + 200) - 200) + 'px',
             left: '-300px'
         },
         {
-            top: (Math.random() * window.innerHeight - 200) + 'px',
+            top: (Math.random() * (window.innerHeight + 200) - 200) + 'px',
             left: window.innerWidth + 'px'
         }
     ], {
-        duration: Math.random() * 3000 + 2000,
+        duration: Math.random() * 2000 + 2000,
         iterations: 1,
         delay: 3000
 
@@ -23,4 +27,4 @@ function animateHedg() {
 }
 // animateHedg();
 
-setInterval(() => {animateHedg()}, Math.random() * 3000 + 5000);
+setInterval(() => {animateHedg()}, Math.random() * 3000 + 4000);
